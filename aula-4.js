3e5fe2b32a66aa8c1cacf9926b4063366b7361bd
Digitar seu nome aqui <83990032+marcoshenriquefc@users.noreply.github.com>
2023-03-14 20:12:40 -0300

    Update aula-4.js

--- aula-4.js
+++ aula-4.js
@@ -1,3 +1,21 @@
-const fruta1 = "banana";
-const fruta2 = "maçã";
-const fruta3 = "laranja";
+let fruta1 = "banana";
+
+console.log(fruta1);
+
+fruta1 = "melancia";
+
+console.log(fruta1);
+
+const nomedopais = "brasil";
+const pontosturisticos = "Praia de Ipanema";
+const capitaldopais = "brasilia";
+const quantidadedeestados = 26;
+const opaísédaamericalatina =  true;
+
+console.log(nomedopais);
+console.log(pontosturisticos);
+console.log(capitaldopais);
+console.log(quantidadedeestados)
+console.log(opaísédaamericalatina);
+
+
